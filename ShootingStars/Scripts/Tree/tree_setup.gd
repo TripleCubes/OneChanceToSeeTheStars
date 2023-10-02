b042be083d20f931c973e7c4374cfbc6b5817e2d
TripleCubes <113813506+TripleCubes@users.noreply.github.com>
2023-10-02 11:14:45 +0700

    Add dialogues

--- ShootingStars/Scripts/Tree/tree_setup.gd
+++ ShootingStars/Scripts/Tree/tree_setup.gd
@@ -15,9 +15,28 @@ func _ready():
 	.ab1("You might only see these kind of stuff once in life").ab0("Mhm").asf1("You plan to make a wish?") \
 	.asf1("Have you seen meteor shower before?")
 
-	#
-	Branch.new("I am thinking of one", true).ab0("Do you?").ab1("It is a secret").ab0("Mhm")
+	Branch.new("I am thinking of one", true).ab0("Do you?").ab1("Maybe I will just wish for a good month") \
+	.ab1("Thinking of a wish is hard").ab0("Mhm").ab0("Last time I see a shooting star I wished for luck") \
+	.ab0("The next day my coworker give me a discount coupon").ab0("So I guess the wish came true") \
+	.ab1("Then I hope my wish will also come true").ab0("I want to wish for something more specific this time tho") \
+	.asf1("How about wish for money?").asf1("How about just wish for more shooting stars?") \
+	.asf1("Can we wish for this shooting star to be a really beautiful one?")
 	OT.gb("You plan to make a wish?").a("I am thinking of one")
+
+	#
+	Branch.new("Nah that is a lame wish", true)
+	OT.gb("How about wish for money?").a("Nah that is a lame wish")
+
+	#
+	Branch.new("That is a nice one", true)
+	OT.gb("How about just wish for more shooting stars?").a("That is a nice one")
+
+	#
+	Branch.new("We will need to make the wish before the shooting stars start", true) \
+	.ab1("I mean who say we cant right?").ab0("Yeah").ab0("Guess that is my wish then")
+	OT.gb("Can we wish for this shooting star to be a really beautiful one?").a("We will need to make the wish before the shooting star start")
+
+
 
 	Branch.new("Only small ones", true).ab1("Same").ab0("Those are nice tho").ab0("The stars are also beautiful") \
 	.asf1("They feel unreal right?").asf1("Sometime the color of the cloud is nice too")
@@ -31,14 +50,14 @@ func _ready():
 	.ab0("I have seen some straight cloud shapes").ab0("I once seen a sheep shaped cloud") \
 	.ab0("Like, it look like a sheep, with horns and stuff").asf1("Sheeps are so cute") \
 	.asf1("Cloud and sheep are both fluffy")
-	OT.gb("Sometime the color of the cloud is nice too").a("Yeah the blue clouds are really nice")
+	OT.gb("Sometime the color of the cloud is nice too").a("Yeah the blue clouds are really pretty")
 
 	#
 	Branch.new("I know right?", true)
 	OT.gb("Sheeps are so cute").a("I know right?")
 
 	Branch.new("Mhm, fluffy stuff are so cute", true).ab0("I have a lot of plushies") \
-	.ab0("I always get the fluffies one").ab1("I dont have plushies but my pillow is really soft").ab0("Me too") \
+	.ab0("I always get the fluffies one") \
 	.ab1("What type of plush do you have?").ab0("I have pony plush, cat plush, a lot of sheep plushs") \
 	.asf1("You like ponies?").asf1("You like cats?").asf1("You like sheeps?")
 	OT.gb("Cloud and sheep are both fluffy").a("Mhm, fluffy stuff are so cute")
@@ -61,8 +80,13 @@ func _ready():
 	.asf1("Um... no").asf1("It is one side from me, I think")
 	OT.gb("My friend like to go there, I just go with her").a("Is she a close friend?")
 
-	#
-	Branch.new("Oh... So, she doesnt work with you?", true)
+	# ->
+	Branch.new("Oh... So, she doesnt work with you?", true).ab1("No. She is indie. She do live stream and stuff") \
+	.ab0("I would like to get into art too").ab1("Why dont you?").ab0("It seem hard to me") \
+	.ab1("I mean everything seem hard at first").ab0("Yeah.") \
+	.ab0("It is just, I have these cool ideas I want to draw,") \
+	.ab0("And when I tried to draw them, they doesnt look as good").ab0("That just make me lost motivation") \
+	.ab1("Well that happen to everyone of us").ab0("Yeah. I guess I just need to keep trying")
 	OT.gb("Um... no").a("Oh... So, she doesnt work with you?")
 
 	#
